a18a6df0718a387cb22d6b48cbfbe183342cc9af
Brianna Thompson <brianna@affordablegeek.net>
2020-06-10 21:49:53 -0400

    Update PA11_1.cpp

--- PA11_1.cpp
+++ PA11_1.cpp
@@ -3,8 +3,10 @@
 //  C++ Program
 //
 //  Created by Brianna Thompson on 11/25/18.
-//  Copyright � 2018 Brianna Thompson. All rights reserved.
-//
+//  Copyright © 2018 Brianna Thompson. All rights reserved.
+// Write a program that asks the user to enter a full name, 
+// First Middle Last, and display the three names on separate 
+// lines and then the initials on a separate line. Trivia – What is Smith's stage name?
 
 #include <stdio.h>
 #include <iostream>
