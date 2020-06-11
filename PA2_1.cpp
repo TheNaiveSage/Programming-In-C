a6ed48bcbf24d151f650a17555fea6a64f55608f
Brianna Thompson <brianna@affordablegeek.net>
2020-06-10 22:31:14 -0400

    Add files via upload

---
+++ PA2_1.cpp
@@ -0,0 +1,26 @@
+/***
+*  PA2-1.cpp
+*  C++ Program
+*
+*  Created by Brianna Thompson on 8/29/18.
+*  Copyright © 2018 Brianna Thompson. All rights reserved.
+* Open CodeBlocks and write a program that creates a rectangle
+* of asterisks. In the program demonstrate that you can use both
+* "one line" comments, using //, and block comments using /*
+***/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Display Rectangle of Asterisk
+    cout << "*******" << endl;
+    cout << "*     *" << endl;
+    cout << "*     *" << endl;
+    cout << "*     *" << endl;
+    cout << "*     *" << endl;
+    cout << "*     *" << endl;
+    cout << "*******" << endl;
+    return 0;
+}
+//Brianna Thompson PA2-1
