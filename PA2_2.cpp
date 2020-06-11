157fc6cfcb9108166ac82557b14fe41346e27022
Brianna Thompson <brianna@affordablegeek.net>
2020-06-10 22:34:47 -0400

    Add files via upload

---
+++ PA2_2.cpp
@@ -0,0 +1,22 @@
+//
+//  PA2-2.cpp
+//  C++ Program
+//
+//  Created by Brianna Thompson on 8/29/18.
+//  Copyright © 2018 Brianna Thompson. All rights reserved.
+// Write a program that has your initials laid out similarly
+// to the output below.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Display Initial-Ception
+    cout << "BBBBB      A     TTTTT" << endl;
+    cout << "B    B    A A      T  " << endl;
+    cout << "BBBBB    AAAAA     T  " << endl;
+    cout << "B    B  A     A    T  " << endl;
+    cout << "BBBBB  A       A   T  " << endl;
+    return 0;
+}
+//Brianna Thompson PA2-2
---
+++ PA2_3.cpp
@@ -0,0 +1,21 @@
+//
+//  PA2-3.cpp
+//  C++ Program
+//
+//  Created by Brianna Thompson on 8/29/18.
+//  Copyright © 2018 Brianna Thompson. All rights reserved.
+// Write a program that shows you understand escape sequence
+// by reproducing the results below
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Display Escape Sequences
+    cout << "The following is enclosed in double quotes: \"Double Quotes\"."  << endl;
+    cout << "\t This is enclosed in single quotes \'Single Quotes\'." << endl;
+    cout<< "\t\t I even know how to back backslashes appear in the output - \\\\." <<endl;
+
+    return 0;
+}
+//Brianna Thompson PA2-3
